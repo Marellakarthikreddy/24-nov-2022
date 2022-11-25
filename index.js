9eec7386de2cfd82dfac97256236dcb76815ac31
Marellakarthikreddy <karthikreddymarella66@gmail.com>
2022-11-25 11:57:31 +0530

    25 nov 2022

--- index.js
+++ index.js
@@ -13,12 +13,11 @@
 		app.use(cors())
 		app.get("/",(req,res)=>{
 			res.send("good morning")
-
 		})
 		app.post("/newData",(req,res)=>{
 			//console.log(req.body)
-			const {name,pin}=req.body
-			console.log(name,pin,"values")
+			const {name,pin,course,gender}=req.body
+			console.log(name,pin,course,gender,"values")
 			res.send("Data added")
 
 		})
